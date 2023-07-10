81b80cb3583366272b5ba4d546c9e030c4b0075a
FNU Arnav <fnuarnav@microsoft.com>
2023-07-10 14:53:23 -0700

    redis in a local container

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ az login
 
 export STORAGE_ACCOUNT_KEY=$(az storage account keys list -g $RG_NAME -n $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
 
-sed "s/STORAGE_SHARE_NAME/$SHARE_NAME/" containerinstance.json > tempjson.json 
+echo $CONTAINER_FILE_NAME
+sed "s/STORAGE_SHARE_NAME/$SHARE_NAME/" $CONTAINER_FILE_NAME > tempjson.json 
 sed "s/STORAGE_ACCOUNT_NAME/$STORAGE_ACCOUNT_NAME/" tempjson.json -i 
 sed "s|STORAGE_ACCOUNT_KEY|$STORAGE_ACCOUNT_KEY|" tempjson.json -i
 sed "s/CONTAINER_GROUP_NAME/$CONTAINER_GROUP_NAME/" tempjson.json -i
